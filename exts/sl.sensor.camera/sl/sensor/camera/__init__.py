748831e9b4295aba8c7c36aaa1829a69f22fbf2a
Matthew Rousseau <matthew.rousseau@stereolabs.com>
2025-02-24 17:21:57 +0100

    Fix ZED SDK 4.2.5 compatibility

--- exts/sl.sensor.camera/sl/sensor/camera/__init__.py
+++ exts/sl.sensor.camera/sl/sensor/camera/__init__.py
@@ -5,6 +5,8 @@ from .ogn import *
 # Any class derived from `omni.ext.IExt` in a top level module (defined in `python.modules` of `extension.toml`) will be
 # instantiated when the extension is enabled and `on_startup(ext_id)` will be called. Later when extension gets disabled
 # on_shutdown() will be called.
+
+
 class SlSensorCameraExtension(omni.ext.IExt):
     # ext_id is the current extension id. It can be used with the extension manager to query additional information,
     # such as where this extension is located in the filesystem.
