c5c806ef7b349a6940ecede39187bb4f7a88b425
Matthew Rousseau <matthew.rousseau@stereolabs.com>
2025-07-22 17:54:23 +0200

    Comment out ZED SDK version

--- exts/sl.sensor.camera.bridge/include/zed_interface_loader.hpp
+++ exts/sl.sensor.camera.bridge/include/zed_interface_loader.hpp
@@ -73,7 +73,7 @@ namespace sl
     class ZedStreamer {
     private:
         LibHandle hLibrary;
-    
+
         typedef int (*GetSDKVersion)(int&, int&, int&);
         typedef bool (*InitStreamerFunc)(int, struct StreamingParameters*);
         typedef int (*StreamRGBFunc)(int, unsigned char*, unsigned char*, long long, float, float, float, float, float, float, float);
@@ -82,7 +82,7 @@ namespace sl
         typedef void (*DestroyInstanceFunc)();
         typedef int* (*GetVirtualCameraIdentifiersFunc)(int*);
         typedef int (*IngestImuFunc)(int, long long, float, float, float, float, float, float, float, float, float, float);
-    
+
         GetSDKVersion get_sdk_version;
         InitStreamerFunc init_streamer;
         StreamRGBFunc stream_rgb;
@@ -91,7 +91,7 @@ namespace sl
         DestroyInstanceFunc destroy_instance;
         GetVirtualCameraIdentifiersFunc get_virtual_camera_identifiers;
         IngestImuFunc ingest_imu;
-    
+
         bool loaded;
 
     public:
@@ -105,7 +105,7 @@ namespace sl
             get_virtual_camera_identifiers = nullptr;
             ingest_imu = nullptr;
         }
-    
+
         ~ZedStreamer() {
             unload();
         }
@@ -124,9 +124,9 @@ namespace sl
             loaded = true;
             return true;
         }
-    
-        bool load_api() 
-        {   
+
+        bool load_api()
+        {
             init_streamer = (InitStreamerFunc)GetFunc(hLibrary, "init_streamer");
             stream_rgb = (StreamRGBFunc)GetFunc(hLibrary, "stream_rgb");
             stream_yuv = (StreamYUVFunc)GetFunc(hLibrary, "stream_yuv");
@@ -134,12 +134,12 @@ namespace sl
             destroy_instance = (DestroyInstanceFunc)GetFunc(hLibrary, "destroy_instance");
             get_virtual_camera_identifiers = (GetVirtualCameraIdentifiersFunc)GetFunc(hLibrary, "get_virtual_camera_identifiers");
             ingest_imu = (IngestImuFunc)GetFunc(hLibrary, "ingest_imu");
-                
+
             loaded = true;
             return true;
         }
-    
-        void unload() 
+
+        void unload()
         {
             if (hLibrary) {
                 CloseLib(hLibrary);
@@ -156,7 +156,7 @@ namespace sl
             get_virtual_camera_identifiers = nullptr;
             ingest_imu = nullptr;
         }
-    
+
         bool isLoaded() const {
             return loaded;
         }
@@ -190,7 +190,7 @@ namespace sl
                 int res = getSDKVersion(major, minor, patch);
                 if (res == 0)
                 {
-                    CARB_LOG_INFO("[ZED] Found SDK v%d.%d.%d", major, minor, patch);
+                    // CARB_LOG_INFO("[ZED] Found SDK v%d.%d.%d", major, minor, patch);
 
                     if (major > ZED_SDK_VERSION_MAJOR) return true;
                     if (major < ZED_SDK_VERSION_MAJOR) return false;
@@ -205,7 +205,7 @@ namespace sl
 
             return false;
         }
-    
+
         bool initStreamer(int streamer_id, struct StreamingParameters* streaming_params) {
             if (!loaded || !init_streamer) {
                 std::cerr << "[ZED] Error with init_streamer function call" << std::endl;
@@ -219,9 +219,9 @@ namespace sl
 
             return init_streamer(streamer_id, streaming_params);
         }
-    
+
         int stream(sl::INPUT_FORMAT input, int streamer_id, unsigned char* left, unsigned char* right,
-                       long long timestamp_ns, float qw, float qx, float qy, float qz, 
+                       long long timestamp_ns, float qw, float qx, float qy, float qz,
                        float lin_acc_x, float lin_acc_y, float lin_acc_z) {
 
             if (input == sl::INPUT_FORMAT::RGB || input == sl::INPUT_FORMAT::BGR)
@@ -243,7 +243,7 @@ namespace sl
                     lin_acc_x, lin_acc_y, lin_acc_z);
             }
         }
-    
+
         void closeStreamer(int streamer_id) {
             if (!loaded || !close_streamer) {
                 std::cerr << "[ZED] Error with close_streamer function call" << std::endl;
@@ -251,7 +251,7 @@ namespace sl
             }
             close_streamer(streamer_id);
         }
-    
+
         void destroyInstance() {
             if (!loaded || !destroy_instance) {
                 std::cerr << "[ZED] Error with destroy_instance function call" << std::endl;
@@ -259,7 +259,7 @@ namespace sl
             }
             destroy_instance();
         }
-    
+
         int* getVirtualCameraIdentifiers(int* size_out) {
             if (!loaded || !get_virtual_camera_identifiers) {
                 std::cerr << "[ZED] Error with get_virtual_camera_identifiers function call" << std::endl;
@@ -267,15 +267,15 @@ namespace sl
             }
             return get_virtual_camera_identifiers(size_out);
         }
-    
-        int ingestIMU(int streamer_id, long long timestamp_ns, float vx, float vy, float vz, 
-                      float lin_acc_x, float lin_acc_y, float lin_acc_z, 
+
+        int ingestIMU(int streamer_id, long long timestamp_ns, float vx, float vy, float vz,
+                      float lin_acc_x, float lin_acc_y, float lin_acc_z,
                       float qw, float qx, float qy, float qz) {
             if (!loaded || !ingest_imu) {
                 std::cerr << "[ZED] Error with ingest_imu function call " << std::endl;
                 return -1;
             }
-            return ingest_imu(streamer_id, timestamp_ns, vx, vy, vz, 
+            return ingest_imu(streamer_id, timestamp_ns, vx, vy, vz,
                              lin_acc_x, lin_acc_y, lin_acc_z, qw, qx, qy, qz);
         }
     };
