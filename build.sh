747a0476ab81ba53b7882b3efbf6e74a1cc2e93b
bvallon <benjamin.vallon@stereolabs.com>
2025-07-23 14:54:24 +0200

    include sl zed in the package

--- build.sh
+++ build.sh
@@ -14,4 +14,6 @@ $SCRIPT_DIR/repo.sh build "$@"
 # Copy build artifacts into exts path for simplicity
 cp -r $BUILD_PATH/bin/* "exts/sl.sensor.camera.bridge/bin/"
 
+cp "exts/sl.sensor.camera.bridge/bin/libsl_zed.so" "$BUILD_PATH/bin/libsl_zed.so"
+
 #rm -rf _compiler _deps _repo
