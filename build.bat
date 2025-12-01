d6d5a6414ffcab1f2b3458a73da4c477d78d70cd
bvallon <benjamin.vallon@stereolabs.com>
2025-12-01 16:16:00 +0100

    update build scripts

--- build.bat
+++ build.bat
@@ -2,14 +2,14 @@
 setlocal enabledelayedexpansion
 set SCRIPT_DIR=%~dp0
 
-set "SL_ZED_VERSION=1.0.0"
+set "SL_ZED_VERSION=4.1.0"
 echo Downloading dependencies version !SL_ZED_VERSION!...
 
-set "BUILD_PATH=_build/windows-x86_64/release/exts/sl.sensor.camera.bridge"
+set "BUILD_PATH=_build/windows-x86_64/release/exts/sl.sensor.camera.bridge/bin"
 set "SL_ZED_DOWNLOAD_URL=https://stereolabs.sfo2.cdn.digitaloceanspaces.com/utils/zed_isaac_sim/!SL_ZED_VERSION!/sl_zed64_windows_x86_64_!SL_ZED_VERSION!.tar.gz"
 
 REM Download the sl_zed library if it doesn't exist
-if not exist "exts\sl.sensor.camera.bridge\bin\sl_zed64.dll" (
+if not exist "%BUILD_PATH%\sl_zed64.dll" (
     echo Downloading sl_zed library...
     powershell -Command "Invoke-WebRequest -Uri '!SL_ZED_DOWNLOAD_URL!' -OutFile 'exts\sl.sensor.camera.bridge\bin\sl_zed64_windows_x86_64_!SL_ZED_VERSION!.tar.gz'"
     if errorlevel 1 (
@@ -35,7 +35,6 @@ call "%SCRIPT_DIR%repo.bat" build %*
 if errorlevel 1 exit /b %ERRORLEVEL%
 
 REM Copy build artifacts
-xcopy /e /i /y "%BUILD_PATH%\bin\*" "exts\sl.sensor.camera.bridge\bin"
-xcopy /e /i /y "%BUILD_PATH%\ogn" "exts\sl.sensor.camera.bridge\ogn"
+xcopy /e /i /y "%BUILD_PATH%\*" "exts\sl.sensor.camera.bridge\bin"
 
-copy /y "exts\sl.sensor.camera.bridge\bin\sl_zed64.dll" "%BUILD_PATH%\bin\sl_zed64.dll" 
+copy /y "exts\sl.sensor.camera.bridge\bin\sl_zed64.dll" "%BUILD_PATH%\sl_zed64.dll" 
