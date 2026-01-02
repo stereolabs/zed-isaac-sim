163f34b936f1e485d4d658444d4f7099c6facf8e
bvallon <>
2026-01-02 14:31:54 +0100

    Fix database version conflict

--- exts/sl.sensor.camera/sl/sensor/camera/ogn/SlCameraStreamerDatabase.py
+++ exts/sl.sensor.camera/sl/sensor/camera/ogn/SlCameraStreamerDatabase.py
@@ -49,7 +49,7 @@ class SlCameraStreamerDatabase(og.Database):
 
     # Imprint the generator and target ABI versions in the file for JIT generation
     GENERATOR_VERSION = (1, 79, 2)
-    TARGET_VERSION = (0, 0, 0)
+    TARGET_VERSION = (2, 181, 8)
 
     # This is an internal object that provides per-class storage of a per-node data dictionary
     PER_NODE_DATA = {}
